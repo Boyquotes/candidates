7d99367c5e31091a3dc5f1483dd4949c93606395
Boyquotes <webmaster@linux-live-cd.org>
2016-03-07 10:31:39 +0100

    Le widget liste bien les candidats grâce à WP_QUERY

--- candidates.php
+++ candidates.php
@@ -118,14 +118,49 @@ class class_widget_candidates_list extends WP_Widget {
         extract($args);
         echo $before_widget;
 ?>
-    <div id="listCandidates">
-      <h3 class="widget-title">Liste des candidats</h3>
-      <ul>
-          <li>
-              <a href="#URL" #FILTRE_egalite(#PRODUIT_ID||#ID||class="selection")>#TITRE</a>
-          </li>
-      </ul>
-    </div>
+<h2> Les candidats </h2>
+<div class="candidat">
+  <?php
+  // 1 Parametrage de la requete dans un tableau $args
+  $args = array(
+    'post_type'  => 'candidat',
+    'posts_per_page' => '3',
+  );
+  // 2. on exécute la query
+  $my_query = new WP_Query($args);
+
+  #LA BOUCLE
+  // 3. on lance la boucle WHILE!
+  if($my_query->have_posts()) : while ($my_query->have_posts() ) : $my_query->the_post();
+  ?>
+    <p>
+      <?php
+          the_title();
+          //echo "</br>DEBUG ID du post :".get_the_ID()." DEBUG</br>";
+          $prenom = get_post_meta(get_the_ID(), '_ma_valeur_prenom', true);
+          if(isset($prenom)){
+              echo " Prénom";
+              echo $prenom;
+              echo "</br>";
+          }
+
+          $heros = get_post_meta(get_the_ID(), '_ma_valeur_heroes', true);
+          if(isset($heros) && $heros != ""){
+              echo " Son héros : ";
+              echo $heros;
+              echo "</br>";
+          }
+          else{
+              echo "Il n'a pas rempli le champs héros";
+          }
+      ?>
+    </p>
+    <?php
+    endwhile;
+    endif;
+    ?>
+    <!-- fin candidat -->
+  </div>
 
 <?php
         echo $after_widget;
